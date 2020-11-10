68cc650431e11241ee676ff3526e2ef29c9723d7
Snekyy <42645840+Snekyy@users.noreply.github.com>
2020-11-10 18:53:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 btn = document.getElementById("email");
 btn.onclick = function() {
-    alert('protonmail : snekyy@protonmail.com\ngmail : snek1337228@gmail.com');
+    alert('protonmail : snekyy@protonmail.com');
 }
